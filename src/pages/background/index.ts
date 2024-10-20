b5e46b6d3dc309ae950f6e03b71a7d1a639a0b52
Joshua Sharp <joshuasfsharp@gmail.com>
2024-10-20 23:48:11 +1100

    wip auth

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,79 @@
 console.log('background script loaded');
+
+export let chromeStorageKeys;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.action) {
+      case "signInWithGoogle": {
+  
+              // remove any old listener if exists
+        chrome.tabs.onUpdated.removeListener(setTokens)
+        const url = request.payload.url;
+  
+        // create new tab with that url
+        chrome.tabs.create({ url: url, active: true }, (tab) => {
+            // add listener to that url and watch for access_token and refresh_token query string params
+            chrome.tabs.onUpdated.addListener(setTokens)
+
+            sendResponse(request.action + " executed")
+        })
+       
+        break
+      }
+  
+      default:
+        break
+    }
+  
+    return true
+  })
+  
+  
+  chromeStorageKeys = {
+    gauthAccessToken: "gauthAccessToken",
+    gauthRefreshToken: "gauthRefreshToken"
+  }
+  
+  
+  const setTokens = async (
+    tabId: number,
+    changeInfo: chrome.tabs.TabChangeInfo,
+    tab: chrome.tabs.Tab
+  ) => {
+  
+    // once the tab is loaded
+    if (tab.status === "complete") {
+      if (!tab.url) return
+      const url = new URL(tab.url)
+  
+      // at this point user is logged-in to the web app
+      // url should look like this: https://my.webapp.com/#access_token=zI1NiIsInR5c&expires_in=3600&provider_token=ya29.a0AVelGEwL6L&refresh_token=GEBzW2vz0q0s2pww&token_type=bearer
+      // parse access_token and refresh_token from query string params
+    //   if (url.origin === "https://my.webapp.com") {
+        const params = new URL(url.href).searchParams;
+        const accessToken = params.get("accessToken");
+              const refreshToken = params.get("refreshToken");
+  
+        if (accessToken && refreshToken) {
+          if (!tab.id) {
+            return
+          }
+  
+          // we can close that tab now
+          await chrome.tabs.remove(tab.id)
+  
+  
+          // store access_token and refresh_token in storage as these will be used to authenticate user in chrome extension
+          await chrome.storage.sync.set({
+            [chromeStorageKeys.gauthAccessToken]: accessToken
+          })
+          await chrome.storage.sync.set({
+            [chromeStorageKeys.gauthRefreshToken]: refreshToken
+          })
+  
+          // remove tab listener as tokens are set
+          chrome.tabs.onUpdated.removeListener(setTokens)
+        }
+    //   }
+    }
+  }
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '@assets/img/logo.svg';
+import { createClient, User } from '@supabase/supabase-js'
+import { chromeStorageKeys } from '../background';
 
-export default function Popup(): JSX.Element {
+const supabaseUrl = "https://qfxxcuboozaccrjdqhzq.supabase.co";
+  const supabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmeHhjdWJvb3phY2NyamRxaHpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzNDQxNTQsImV4cCI6MjA0NDkyMDE1NH0.2FiScM9ps-SQFtN2wODLr9dLYgDLzowKGrTUtvEDv9k';
+
+export default function Popup() {
+
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+   getCurrentUser().then((data) => {
+    setIsLoading(false);
+    setUser(data?.user ?? null);
+   });
+  }, []);
+
+  const loginWithGoogle = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+      })
+
+      if (error) {
+        console.log("Error: ", error);
+      }
+
+      await chrome.runtime.sendMessage({
+        action: "signInWithGoogle",
+        payload: { url: data.url } // url is something like: https://[project_id].supabase.co/auth/v1/authorize?provider=google
+      });
+  }
+
+const getCurrentUser = async (): Promise<null | {
+  user: User;
+  accessToken: string;
+  }> => {
+    if (!chromeStorageKeys) {
+      console.log("chromeStorageKeys is not defined");
+      return null;
+    }
+
+  const gauthAccessToken = (
+    await chrome.storage.sync.get(chromeStorageKeys?.gauthAccessToken)
+  )[chromeStorageKeys?.gauthAccessToken];
+
+  const gauthRefreshToken = (
+    await chrome.storage.sync.get(chromeStorageKeys.gauthRefreshToken)
+  )[chromeStorageKeys.gauthRefreshToken];
+
+  if (gauthAccessToken && gauthRefreshToken) {
+    try {
+      // set user session from access_token and refresh_token
+      const resp = await supabase.auth.setSession({
+        access_token: gauthAccessToken,
+        refresh_token: gauthRefreshToken,
+      });
+
+      const user = resp.data?.user;
+      const supabaseAccessToken = resp.data.session?.access_token;
+
+      if (user && supabaseAccessToken) {
+        return { user, accessToken: supabaseAccessToken };
+      }
+    } catch (e: any) {
+      console.error("Error: ", e);
+    }
+  }
+
+  return null;
+}
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
+      {isLoading &&
         <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
+      }
+
+      {!isLoading && user && 
+        <div>
+          <p>Logged in as {user.email}</p>
+        </div>
+      }
+
+      {!isLoading && !user &&
+      <header className="flex flex-col items-center justify-center text-white">
         <p>
           Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
         </p>
@@ -18,7 +103,10 @@ export default function Popup(): JSX.Element {
           Learn React!
         </a>
         <p>Popup styled with TailwindCSS!</p>
+
+        <button onClick={loginWithGoogle}>Login with Google</button>
       </header>
+}
     </div>
   );
 }
